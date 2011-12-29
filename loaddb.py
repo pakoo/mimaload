31bab28228c995c8061d3d22cb0a876eda49d6c1
oucena <oucena@gmail.com>
2011-12-29 08:06:13 -0500

    add mysql

--- loaddb.py
+++ loaddb.py
@@ -1,22 +1,47 @@
+# -*- coding: utf-8 -*-
 import pprint
-mimi=open('tianya_1.txt','r')
-data=mimi.readlines()
-print 'data len:',len(data)
-for p in data[151000:152000]:
-    p=p[:-2]
-    p=p.split(' ')
-    p=[a for a in p if a]
-    if len(p)==2:
-        name=p[0]
-        pwd=p[1]
-        print name,pwd
-    elif len(p)==3:
-        name=p[0]
-        pwd=p[1]
-        email=p[2] 
-        print name,pwd,email
-    else:
-        print name,pwd
-    	print p
-    #print p.decode('gbk')
-    #print p.decode('utf-8').encode('gbk'),type(p)
+import MySQLdb
+from MySQLdb.cursors import DictCursor
+import time
+
+db=MySQLdb.connect(host='localhost',user='root',passwd='123456',port=3306,db='tianya')
+cur=db.cursor()
+#cur.execute("""
+#select * from user;
+#"""
+#)
+#print 'user:',cur.fetchall()
+
+def insert_user(name,pwd,email='1'):
+    try :
+        sql="""insert into user(name,password,email) values (%s,%s,%s)"""%(repr(name),repr(pwd),repr(email))
+        #print 'sql:',sql
+        cur.execute(sql)
+        db.commit()
+    except :
+        print sql
+def parser(filename):
+    begin_time=time.time()
+    mimi=open(filename,'r')
+    data=mimi.readlines()
+    print 'data len:',len(data)
+    for p in data:
+        p=p[:-2]
+        p=p.split(' ')
+        p=[a for a in p if a]
+        if len(p)==2:
+            name=p[0]
+            pwd=p[1]
+            insert_user(name,pwd) 
+            #print 'username:',name,'password:',pwd
+        elif len(p)==3:
+            name=p[0]
+            pwd=p[1]
+            email=p[2] 
+            insert_user(name,pwd,email) 
+            #print 'username:',name,'password:',pwd,'email:',email
+    print 'total time=',time.time()-begin_time
+
+if __name__ == '__main__':
+    #insert_user('test','123456')
+    parser('./tianyadb/tianya_1.txt')
